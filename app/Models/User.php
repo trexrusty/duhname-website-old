bdf9222f726bc0979f5f59b0bac0c9f0ae017778
trexrusty <51139052+trexrusty@users.noreply.github.com>
2025-01-14 15:57:46 +0000

    dev bar

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Spatie\Permission\Traits\HasRoles;
+
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable, HasUuids;
+    use HasFactory, Notifiable, HasUuids, HasRoles;
 
     protected $table = 'users';
     public $incrementing = false;
